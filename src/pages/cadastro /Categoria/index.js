43734286cad82d282d29f198a8e30ef8bd1c8bb5
Marcos <marcozzymandias@gmail.com>
2020-08-01 21:02:28 -0300

    aula 05 44min

--- src/pages/cadastro /Categoria/index.js
+++ src/pages/cadastro /Categoria/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/Footer/FormField';
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -11,29 +12,15 @@ function CadastroCategoria() {
     cor: '',
   };
 
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(chave, valor) {
-    // chave: nome, descrição, bla, bli
-    setValues({
-      ...values,
-      [chave]: valor, // nome: 'valor'
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value,
-    );
-  }
 
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
-    ? 'http://localhost:8080/categorias'
-    : 'https://devmarcosflix.herokuapp.com/categorias';
-    // 
+      ? 'http://localhost:8080/categorias'
+      : 'https://devmarcosflix.herokuapp.com/categorias';
+    //
     // fetch(URL_TOP);
     //   .then(async (respostaDoServidor) => {
     //     const resposta = await respostaDoServidor.json();
@@ -42,28 +29,28 @@ function CadastroCategoria() {
     //     ]);
     //   });
 
-  setTimeout(() => {
-    setCategorias([
-      ...categorias,
-      {
-        categorias: [
-          {
-            id: 1,
-            nome: 'Front End',
-            descricao: 'Uma categoria bacana',
-            cor: '#cbd1ff',
-          },
-          {
-            id: 2,
-            nome: 'Back End',
-            descricao: 'Outra categoria bacana',
-            cor: '#cbd1ff',
-          },
+    //   setTimeout(() => {
+    //     setCategorias([
+    //       ...categorias,
+    //       {
+    //         categorias: [
+    //           {
+    //             id: 1,
+    //             nome: 'Front End',
+    //             descricao: 'Uma categoria bacana',
+    //             cor: '#cbd1ff',
+    //           },
+    //           {
+    //             id: 2,
+    //             nome: 'Back End',
+    //             descricao: 'Outra categoria bacana',
+    //             cor: '#cbd1ff',
+    //           },
 
-        ],
-      },
-    ]);
-  }, 4 * 1000);
+  //         ],
+  //       },
+  //     ]);
+  //   }, 4 * 1000);
   }, []);
 
   return (
@@ -80,7 +67,7 @@ function CadastroCategoria() {
           values,
         ]);
 
-        setValues(valoresIniciais);
+        clearForm();
       }}
       >
 
@@ -119,8 +106,8 @@ function CadastroCategoria() {
 
       <ul>
         {categorias.map((categoria) => (
-          <li key={`${categoria.nome}`}>
-            {categoria.nome}
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
